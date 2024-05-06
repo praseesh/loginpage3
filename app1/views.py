9f6f94ae1b62ab9dbeed356db1e8781dadd786ac
Praseesh <praseeshprasee@gmail.com>
2024-05-06 17:14:33 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate
+# Create your views here.
+
+def home(request):
+    return HttpResponse('hello world')
+
+def login_user(request):
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:    
+            return render (request, 'home.html')
+        else:
+            return redirect('login_user')
+    return ('login_user')
