98f6ce5cb182b1b69fdd67a8897cfe275e95ce83
Praseesh <praseeshprasee@gmail.com>
2024-05-07 11:50:41 +0530

    login

--- app1/urls.py
+++ app1/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('home/', views.home),
-    path('login/', views.login_user, name="login")
+    path('login', views.login_user, name="login")
 ]
