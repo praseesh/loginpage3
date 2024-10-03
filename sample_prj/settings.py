d18719b27fb5a5afb582df2929ccabaf6a55586c
Praseesh <150511158+praseesh@users.noreply.github.com>
2024-10-03 21:22:57 +0530

    Update settings.py

--- sample_prj/settings.py
+++ sample_prj/settings.py
@@ -7,9 +7,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-l!wdh(8jq9wodn@1w*qo%(px73r7_i5@*d^h6)wr3+qslhdgl*'
 
